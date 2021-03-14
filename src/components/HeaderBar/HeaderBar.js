94f800d8d8ab9104fd0c7ae77ac7f10beb7f5c4b
Henry Wang <hwang94@hotmail.com>
2021-03-13 21:25:01 -0800

    signout button

--- src/components/HeaderBar/HeaderBar.js
+++ src/components/HeaderBar/HeaderBar.js
@@ -10,7 +10,9 @@ import {
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
-import {NavLink} from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import {NavLink} from 'react-router-dom';
+import firebase from '../../config/firebase';
 
 const useStyles = makeStyles({
   headerContainer: {
@@ -46,6 +48,12 @@ const HeaderBar = () => {
     setAnchorEl(event.currentTarget);
   };
 
+  const logOut = async (event) => {
+    event.preventDefault();
+    const response = firebase.logout();
+    console.log(response);
+  };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -70,6 +78,9 @@ const HeaderBar = () => {
                   </IconButton>
                 </NavLink>
               </div>
+              <Button variant="contained" color="secondary" onClick={logOut}>
+                Signout
+              </Button>
           <IconButton
             aria-controls='simple-menu'
             aria-haspopup='true'
