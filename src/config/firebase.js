31710b884ff20ae24167bdd046a4e274cf8f9e80
Henry Wang <hwang94@hotmail.com>
2021-03-13 17:41:22 -0800

    added env file

---
+++ src/config/firebase.js
@@ -0,0 +1,39 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const config = {
+    apiKey: process.env.REACT,
+    projectId: process.env.PROJECT_ID,
+    authDomain: process.env.AUTH_DOMAIN,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+class Firebase {
+    constructor() {
+        firebase.initializeApp(config)
+		this.auth = firebase.auth()
+    }
+
+    login(email, password) {
+		return this.auth.signInWithEmailAndPassword(email, password)
+	}
+
+	logout() {
+		return this.auth.signOut()
+	}
+
+	register(email, password) {
+		this.auth.createUserWithEmailAndPassword(email, password).then(function() {
+			let user = this.auth.currentUser;
+			console.log(user);
+			return user;
+		}, function(error) {
+			console.log(error.message);
+			return error.message;
+		});
+	}
+
+}
+
+export default new Firebase()
